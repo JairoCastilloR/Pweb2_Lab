b6160f4d37135f10e158d31d6eedea0ab42c08d5
Jairo <jcastillo@unsa.edu.pe>
2020-06-14 15:07:58 -0400

    After a lot test, I'm going to for to easy

--- cgi-bin/question.py
+++ cgi-bin/question.py
@@ -6,23 +6,21 @@ import json
 conection = sqlite3.connect('imdb.db')
 c = conection.cursor()
 
-names = "SELECT name FROM actor"
+names = "SELECT * FROM actor"
 #movies = "SELECT moviesID,|| ','|| title || ' is of the year ' || year AS "Movies at year ",score ||' with those votes ' || votes AS "Movies Scores " FROM movie"
 #I put a variable extremely large for test
-casting = "SELECT movieid , actorid FROM casting"
-movie =  "SELECT title, year, score, votes  FROM movies"
-complete = casting + movie + names 
+#casting = "SELECT movieid , actorid FROM casting"
+movie =  "SELECT * FROM movies"
+
 data = []
 
-for row in c.execute(casting):
+for row in c.execute(movie):
     data.append({
         'id_movie' : row[0],
-        'id_actor' : row[1],
-        #'name_movie' : row[2],
-        #'year' : row[3],
-        #'score' : row[4],
-        #'votes' : row[5],
-        #'name_actor' : row[6]
+        'name_movie' : row[1],
+        'year' : row[2],
+        'score' : row[3],
+        'votes' : row[4],
         })
     print(row)
 
