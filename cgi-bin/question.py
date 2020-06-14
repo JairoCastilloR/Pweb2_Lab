72a5d9ffb8f0c270af2358abcfecfa9fa33bd03b
Jairo <jcastillo@unsa.edu.pe>
2020-06-14 15:35:53 -0400

    More and more errors

--- cgi-bin/question.py
+++ cgi-bin/question.py
@@ -10,10 +10,11 @@ names = "SELECT * FROM actor"
 #movies = "SELECT moviesID,|| ','|| title || ' is of the year ' || year AS "Movies at year ",score ||' with those votes ' || votes AS "Movies Scores " FROM movie"
 #I put a variable extremely large for test
 #casting = "SELECT movieid , actorid FROM casting"
-movie =  "SELECT * FROM movies"
+movie =  "SELECT * FROM movie"
 
 data = []
-
+data1 = []
+   
 for row in c.execute(movie):
     data.append({
         'id_movie' : row[0],
@@ -22,9 +23,14 @@ for row in c.execute(movie):
         'score' : row[3],
         'votes' : row[4],
         })
-    print(row)
 
+for row1 in c.execute(names):
+    data1.append({
+        'id_actor' : row1[0],
+        'name_actor' : row[1]
+        })
+dataComplete = data + data1 
 conection.close()
 
 print("Content-type :application/json\n\n")
-print(json.dumps(data))
+print(json.dumps(dataComplete))
